40074bb5aba5711a16a27254ba4530dbe191fc2f
Lennart Eichhorn <lennart@madmanfred.com>
2022-06-16 14:41:03 +0200

    Add daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 module.exports = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx}'],
-    plugins: [require('daisyui')],
-  };
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  plugins: [require("daisyui")],
+  themes: [
+    {
+      mytheme: {
+        "primary": "#303030",
+        "secondary": "#3584e4",
+        "accent": "#3584e4",
+        "neutral": "#363636",
+        "base-100": "#242424",
+        "info": "#3a3a3a",
+        "success": "#3584e4",
+        "warning": "#e7715c",
+        "error": "#c01c28",
+      },
+    },
+    "dark",
+    "light",
+  ],
+}
